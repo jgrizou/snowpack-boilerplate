e00f3fd254afd1e0d1d13e29b95948f4f2ed7362
Jonathan Grizou <jonathan.grizou@gmail.com>
2021-02-22 22:21:52 +0000

    deploy test again

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+  
   mount: {
-    // get everything from src to build/
     src: '/',
   },
-
+ 
   buildOptions: {
-    out: "./docs",
-    baseUrl: "https://jgrizou.github.io/snowpack-boilerplate/"
-  }
+    baseUrl: '/snowpack-boilerplate',
+    out: './docs',
+  },
+  
 };
