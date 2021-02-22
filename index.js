318a795a77cfa70c5ea94651320585b1ab37829f
Jonathan Grizou <jonathan.grizou@gmail.com>
2021-02-22 18:43:51 +0000

    deploy second attempts

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-// my-first-snowpack/index.js
-import { helloWorld } from './hello-world.js';
-
-helloWorld();
-
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas'), {
-  resize: true,
-  useWorker: true,
- })({ particleCount: 200, spread: 200 });
+// legacy, unsued but can't delete
--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,11 @@
 module.exports = {
+  mount: {
+    // get everything from src to build/
+    src: '/',
+  },
+
   buildOptions: {
-    out: "./docs"
+    out: "./docs",
+    baseUrl: "https://jgrizou.github.io/snowpack-boilerplate/"
   }
 };
